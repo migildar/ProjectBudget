03a95d986b9fe149b24abdfd0849e8b96a885361
Минигулов Ильдар <IMINIGU1@sf-elabuga.ru>
2023-07-04 09:33:03 +0300

    добавление цикла, оптимизация

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let money = prompt('Ваш бюджет на месяц?'),
+let money = +prompt('Ваш бюджет на месяц?'),
     time = prompt('Введите дату в формате YYYY-MM-DD');
 
 const appData = {
-    money: money,
+    budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
@@ -10,14 +10,18 @@ const appData = {
     savings: false
 };
 
-let answerFirstQuestion1 = prompt('Введите обязательную статью расходов в этом месяце'),
-    answerFirstQuestion2 = prompt('Во сколько обойдется?'),
+for (let i = 0; i < 2; i++) {
+    let answerFirstQuestion = prompt('Введите обязательную статью расходов в этом месяце'),
+        answerSecondQuestion = prompt('Во сколько обойдется?');
 
-    answerFirstQuestion3 = prompt('Введите обязательную статью расходов в этом месяце'),
-    answerFirstQuestion4 = prompt('Во сколько обойдется?');
+    if ((typeof(answerFirstQuestion)) === 'string' && (typeof(answerFirstQuestion)) != null && (typeof(answerSecondQuestion)) != null
+        && answerFirstQuestion != '' && answerSecondQuestion != '' && answerFirstQuestion.length < 50) {
+        appData.expenses[answerFirstQuestion] = answerSecondQuestion;
+    } else {
+        i--;
+    }
+}    
 
-appData.expenses[answerFirstQuestion1] = answerFirstQuestion2;
-appData.expenses[answerFirstQuestion3] = answerFirstQuestion4;
-
-alert(appData.money / 30);
+appData.moneyPerDay = appData.budget / 30;
+alert(`Ежедневный бюджет: ${appData.moneyPerDay}`);
 
